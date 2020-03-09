1109bfb7c23d0ce7ab2fe431a8e40c832837363c
j721 <juliadvu@gmail.com>
2020-03-09 15:39:54 -0500

    Updates to navigation bar

--- js/index.js
+++ js/index.js
@@ -47,3 +47,11 @@ circle.setAttribute('src', siteContent["cta"]["img-src"])
 document.querySelector('h1').textContent ="DOM is Awesome"
 
 document.querySelector('button').textContent ="Get Started"
+
+const navigation =document.querySelectorAll("a");
+navigation[0].textContent =siteContent["nav"]["nav-item-1"];
+navigation[1].textContent =siteContent["nav"]["nav-item-2"];
+navigation[2].textContent =siteContent["nav"]["nav-item-3"];
+navigation[3].textContent =siteContent["nav"]["nav-item-4"];
+navigation[4].textContent =siteContent["nav"]["nav-item-5"];
+navigation[5].textContent =siteContent["nav"]["nav-item-6"];
